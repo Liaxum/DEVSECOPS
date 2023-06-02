0254fccc3eb6f1e08b0215f29f3f10ad332c55c7
Romain TEXIER <46653730+Liaxum@users.noreply.github.com>
2023-06-02 16:04:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const params = new URLSearchParams(window.location.search);
 const titleElement = document.getElementById('title');
 const name = params.get('name');
-
-titleElement.innerHTML = `Hello ${name}`;
+if (name != null) titleElement.innerHTML = `Hello ${name}`;
+else titleElement.innerHTML = 'Hello world!"
